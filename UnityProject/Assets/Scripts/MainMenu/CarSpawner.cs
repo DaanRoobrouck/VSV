ab925d30af50c109231740d7991316e43cd0ac1e
MatisseWaghemans <waghemans.matisse@gmail.com>
2020-03-26 19:35:58 +0100

    Improved main screen to more vehicles can spawn
    title

---
+++ UnityProject/Assets/Scripts/MainMenu/CarSpawner.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSpawner : MonoBehaviour
+{
+    public GameObject SportsCar;
+    public GameObject Truck;
+    public GameObject Bike;
+    public GameObject IcecreamTruck;
+    public GameObject RedCar;
+
+
+    public Transform SpawnPoint;
+
+
+    private float spawnTimer;
+    private float despawnTimer;
+
+
+    private GameObject currentCar;
+    private int carNumber;
+    private void Start()
+    {
+        SpawnPoint = this.gameObject.transform;
+        SelectCar();
+        SpawnCar();
+    }
+    void Update()
+    {
+        spawnTimer += Time.deltaTime;
+        if(spawnTimer >= 4)
+        {
+            SelectCar();
+            SpawnCar();
+            spawnTimer = 0;
+        }
+    }
+
+    private void SelectCar()
+    {
+        carNumber = UnityEngine.Random.Range(0, 5);
+        switch (carNumber)
+        {
+            case 0:
+                currentCar = SportsCar;
+                break;
+            case 1:
+                currentCar = Truck;
+                break;
+            case 2:
+                currentCar = Bike;
+                break;
+            case 3:
+                currentCar = IcecreamTruck;
+                break;
+            case 4:
+                currentCar = RedCar;
+                break;
+        }
+    }
+
+    private void SpawnCar()
+    {
+        Instantiate(currentCar, SpawnPoint);
+    }
+}
---
+++ UnityProject/Assets/Scripts/MainMenu/DespawnScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DespawnScript : MonoBehaviour
+{
+    public float timer;
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer > 4)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
