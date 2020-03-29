27ade7dc8ed5687170c61c7a5f24ef6a6d11a991
DaanRoobrouck <54914795+DaanRoobrouck@users.noreply.github.com>
2020-03-29 13:53:03 +0200

    Update UI

---
+++ UnityProject/Assets/Scripts/UI/DestinationManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+using System.Linq;
+
+
+public class DestinationManager : MonoBehaviour
+{
+    [SerializeField] private BoxCollider[] _destinations;
+    public BoxCollider[] CurrentDestinations { get; set; }
+    public Transform CurrentDestination { get; set; }
+    private int _currentDestinationIndex = 0; 
+
+    private Random _random;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _random = new Random();
+        GetRandomDestinations(_destinations, 3);
+        SetDestination();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void GetRandomDestinations(BoxCollider[] destinations, int amountOfDestinations)
+    {
+        CurrentDestinations = new BoxCollider[3];
+        for (int i = 0; i < amountOfDestinations; i++)
+        {
+            int index = _random.Next(0, _destinations.Length);
+
+            while (CurrentDestinations.Contains(_destinations[index]))
+            {
+                index = _random.Next(0, _destinations.Length);
+            }
+            CurrentDestinations[i] = _destinations[index];
+        }
+        Debug.Log(CurrentDestinations);
+    }
+
+    private void SetDestination()
+    {
+        CurrentDestination = CurrentDestinations[_currentDestinationIndex].transform;
+        //event to update UI (also call it when updating the destination)
+    }
+}
