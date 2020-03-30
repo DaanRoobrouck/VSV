d2bb4d802d417644dcfea905e9630751024a88fb
Maxim Eylenbosch <55977182+MaximEylenbosch@users.noreply.github.com>
2020-03-30 19:58:18 +0200

    ADDED: ScoreManager script

---
+++ UnityProject/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    private float _score = 0;
+    private int _concentrationScore = 0;
+    private int _currentStreak = 1;
+
+    //The concentrationScore and currentStreak values can be modified in the SituationController
+    public int ConcentrationScore { get => _concentrationScore; set => _concentrationScore = value; }
+    public int CurrentStreak { get => _currentStreak; set => _currentStreak = value; }
+
+    [SerializeField] private int _mistake = 1;
+    [SerializeField] private int _correct = 1;
+
+    public void AddScore(float time)
+    {
+        _score += Mathf.Pow((_correct * (1 / time)), _currentStreak);
+    }
+
+    public void SubtractScore(int tries)
+    {
+        _score -= (_mistake * tries);
+    }
+
+    public void SendScore(string name, string email)
+    {
+        //Some logic that takes the name (string) of the player and the email (string) of the teacher
+    }
+
+    public void CalculateFinalScore()
+    {
+        _score = _score * _concentrationScore;
+    }
+}
