1ddd942039a076ff59d313e6ad25d52eed96bb5b
MatisseWaghemans <waghemans.matisse@gmail.com>
2020-04-02 15:57:08 +0200

    Added Level selection and Settings
    title

--- UnityProject/Assets/Scripts/MainMenu/UIFunctions.cs
+++ UnityProject/Assets/Scripts/MainMenu/UIFunctions.cs
@@ -4,16 +4,42 @@ using UnityEngine;
 
 public class UIFunctions : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject settingsMenu;
+    [SerializeField]
+    private GameObject mainMenu;
+    [SerializeField]
+    private GameObject levelSelectMenu;
+
+    public Animator startButtonAnim;
+
     public void OpenSVSLink()
     {
         Application.OpenURL("https://www.verkeeropschool.be/");
     }
     public void OpenSettings()
     {
-        Debug.Log("Opening settings");
+        mainMenu.SetActive(false);
+        settingsMenu.SetActive(true);
+    }
+    public void CloseSettings()
+    {
+        settingsMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+    public void CloseLevelSelect()
+    {
+        levelSelectMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+    public void OpenLevelSelect()
+    {
+        levelSelectMenu.SetActive(true);
+        mainMenu.SetActive(false);
     }
     public void Play()
     {
         Debug.Log("Starting game");
+        startButtonAnim.SetTrigger("StartPressed");
     }
 }
