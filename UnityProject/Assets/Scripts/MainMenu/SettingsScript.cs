a5f065cbb060494c85dd846517962f54ec100296
MatisseWaghemans <waghemans.matisse@gmail.com>
2020-04-07 16:13:10 +0200

    changed settings
    title

---
+++ UnityProject/Assets/Scripts/MainMenu/SettingsScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsScript : MonoBehaviour
+{
+    public Slider VolumeSlider;
+    public Slider SensitivitySlider;
+
+    public AudioSource audioSource;
+    public FirstPersonAIO FP;
+
+    private void Update()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        audioSource.volume = VolumeSlider.value;
+        FP.mouseSensitivity = SensitivitySlider.value * 3;
+    }
+}
