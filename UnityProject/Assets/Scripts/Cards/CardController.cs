eca8064417685abd5eb295fc1811ac2b74591951
DaanRoobrouck <54914795+DaanRoobrouck@users.noreply.github.com>
2020-04-08 18:07:00 +0200

    Update Traffic System

---
+++ UnityProject/Assets/Scripts/Cards/CardController.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Situation { A, B, C, D, E}
+public class CardController : MonoBehaviour
+{
+    [SerializeField] private GameObject _cardHolder;
+    private FirstPersonAIO _player;
+
+    [SerializeField] private List<int> _cardIndexOrder;
+    [SerializeField] private List<CardCreator> _scriptableCards;
+    private List<GameObject> _cards;
+    public List<Card> PlayerOrder;
+
+    public Situation AssignedSituation;
+
+    private SituationController _situationController;
+
+    [SerializeField] private GameObject _cardPrefab;
+    [SerializeField] private GameObject _panelCardHolder;
+
+    public BoxCollider Collider;
+
+    private bool _hidecards = false;
+    private void Start()
+    {
+        _player = FindObjectOfType<FirstPersonAIO>();
+        _situationController = GetComponent<SituationController>();
+
+        _cards = new List<GameObject>();
+
+        Collider = this.GetComponent<BoxCollider>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            ShowCards();
+
+            Debug.Log("In");
+            //animatie, kaarten sliden in
+            LeanTween.moveY(_cardHolder, 125, 0.5f);
+            
+            _player.lockAndHideCursor = false;
+            _player.enableCameraMovement = false;
+            Cursor.visible = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            HideCards();
+
+            //animatie, kaarten sliden uit
+            LeanTween.moveY(_cardHolder, -125, 0.5f);
+
+            FreezePlayer(true);
+        }
+    }
+
+    public void FreezePlayer(bool state)
+    {
+        if (!state)
+        {
+            _player.enableCameraMovement = true;
+            _player.lockAndHideCursor = true;
+            Cursor.visible = false;
+        }
+        else
+        {
+            _player.enableCameraMovement = false;
+            _player.lockAndHideCursor = false;
+            Cursor.visible = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CheckOrder();
+        }
+
+        if (_hidecards)
+        {
+            foreach (GameObject cardObject in _cards)
+            {
+                _cards.Remove(cardObject);
+                Destroy(cardObject.gameObject);
+            }
+            if (_cards.Count == 0)
+            {
+                _hidecards = false;
+            }
+        }
+    }
+
+    private void ShowCards()
+    {
+        for (int i = 0; i < _cardIndexOrder.Count; i++)
+        {
+            GameObject cardObject = Instantiate(_cardPrefab, _panelCardHolder.transform);
+            Card card = cardObject.GetComponent<Card>();
+            card.ScriptableCard = _scriptableCards[i];
+            card.Controller = this;
+
+            _cards.Add(cardObject);
+        }
+    }
+
+    private void HideCards()
+    {
+        _hidecards = true;
+    }
+
+    public void CheckOrder()
+    {
+        int incorrect = 0;
+        for (int i = 0; i < PlayerOrder.Count; i++)
+        {
+            if (PlayerOrder[i].Index == _cardIndexOrder[i])
+            {
+                Debug.Log("Juiste selectie");
+                PlayerOrder[i].Btn.image.color = Color.green;
+            }
+            else
+            {
+                Debug.Log("Onjuiste selectie");
+                PlayerOrder[i].Btn.image.color = Color.red;
+                incorrect++;
+            }
+        }
+        if (incorrect == 0)
+        {
+            _hidecards = true;
+            _situationController.CorrectSequence = true;
+            Collider.enabled = false;
+
+            FreezePlayer(false);
+        }
+    }
+}
---
+++ UnityProject/Assets/Scripts/Traffic System/TrafficLightBehavior.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TrafficLightColor
+{
+    Red,
+    Orange,
+    Green
+}
+
+public enum TrafficLightRotation
+{
+    A, B
+}
+
+
+public class TrafficLightBehavior : MonoBehaviour
+{
+
+    [Header("Traffic Light Configuration")]
+    [SerializeField] private Material[] _lightMaterials;
+    [Tooltip("We decide which traffic lights are parallel with each other so they will have the same lights lit up. ")]
+    [SerializeField] private TrafficLightRotation _lightRotation;
+
+    [SerializeField] private bool _hasPedestrianLight;
+
+    
+    [Range(5f,25f)][SerializeField]private int _redAndGreenTimer;
+    [Range(2f,5f)][SerializeField]private int _orangeTimer;
+    private MeshRenderer _renderer;
+
+    private int _blackLightIndex = 1;
+    private int _redLightIndex = 2;
+    private int _orangeLightIndex = 3;
+    private int _greenLightIndex = 4;
+    [Tooltip("Gets assigned at runtime")]
+    [SerializeField] private Material[] _currentLightMaterials;
+
+    void Start()
+    {
+        _renderer = this.GetComponent<MeshRenderer>();
+        _currentLightMaterials = _renderer.sharedMaterials;
+        switch (_lightRotation)
+        {
+            case TrafficLightRotation.A:
+                StartCoroutine(RedLight());
+                break;
+            case TrafficLightRotation.B:
+                StartCoroutine(GreenLight());
+                break;
+        }
+    }
+   
+    private void OnTriggerEnter(Collider other)
+    {
+       // if(other.tag =="Player")
+       // {
+       ////   _currentLightMaterials[4] = MaterialBlackLight;
+       ////   _currentLightMaterials[3] = MaterialOrangeLight;
+       ////   _currentLightMaterials[2] = MaterialBlackLight;
+       //     _isOrange = true;
+       //     _renderer.materials = _currentLightMaterials;
+       // }
+    }
+
+    private IEnumerator GreenLight()
+    {
+        _currentLightMaterials[_redLightIndex] = _lightMaterials[_blackLightIndex];
+        _currentLightMaterials[_orangeLightIndex] = _lightMaterials[_blackLightIndex];
+        _currentLightMaterials[_greenLightIndex] = _lightMaterials[_greenLightIndex];
+        _renderer.sharedMaterials = _currentLightMaterials;
+
+        yield return new WaitForSeconds(_redAndGreenTimer - _orangeTimer);
+        StartCoroutine(OrangeLight());
+    }
+    private IEnumerator OrangeLight()
+    {
+        _currentLightMaterials[_redLightIndex] = _lightMaterials[_blackLightIndex];
+        _currentLightMaterials[_orangeLightIndex] = _lightMaterials[_orangeLightIndex];
+        _currentLightMaterials[_greenLightIndex] = _lightMaterials[_blackLightIndex];
+        _renderer.sharedMaterials = _currentLightMaterials;
+        yield return new WaitForSeconds(_orangeTimer);
+        StartCoroutine(RedLight());
+    }
+    private IEnumerator RedLight()
+    {
+        _currentLightMaterials[_redLightIndex] = _lightMaterials[_redLightIndex];
+        _currentLightMaterials[_orangeLightIndex] = _lightMaterials[_blackLightIndex];
+        _currentLightMaterials[_greenLightIndex] = _lightMaterials[_blackLightIndex];
+        _renderer.sharedMaterials = _currentLightMaterials;
+        yield return new WaitForSeconds(_redAndGreenTimer);
+        StartCoroutine(GreenLight());
+    }
+}
