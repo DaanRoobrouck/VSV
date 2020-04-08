105d4567270d43e97d1581ae9dc762f07405b562
DaanRoobrouck <54914795+DaanRoobrouck@users.noreply.github.com>
2020-04-08 18:40:29 +0200

    Update Traffic

---
+++ UnityProject/Assets/Scripts/Waypoints/CarNavigator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarNavigator : MonoBehaviour
+{
+    //controller
+
+    public CarWaypoint currentWaypoint;
+    private Vector3 _destination;
+    private bool _reachedDestination;
+
+    [Range(3f,5f)][SerializeField] private float _speed;
+
+    private void Awake()
+    {
+        //assign controller
+    }
+
+    void Start()
+    {
+        SetDestination(currentWaypoint.GetPosition());
+    }
+
+    void Update()
+    {
+        float distanceToWaypoint = Vector3.Distance(this.transform.position, _destination);
+        if (distanceToWaypoint <= 0.5f)
+        {
+            currentWaypoint = currentWaypoint.PreviousWaypoint;
+
+                SetDestination(currentWaypoint.GetPosition());
+        }
+        this.transform.LookAt(_destination);
+        this.transform.position = Vector3.MoveTowards(this.transform.position, _destination, Time.deltaTime * _speed);
+    }
+
+    private void SetDestination(Vector3 destination)
+    {
+        _destination = destination;
+        transform.LookAt(currentWaypoint.transform);
+        //_reachedDestination = false;
+    }
+}
