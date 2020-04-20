c8956369f660f2ae43006aff06136d2c143d1ea8
MatisseWaghemans <55841549+MatisseWaghemans@users.noreply.github.com>
2020-04-20 09:15:52 +0200

    Pijltje toegevoegd, maar beweeggt nog niet juist

---
+++ UnityProject/Assets/Scripts/UI/InGame/ArrowLookat.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowLookat : MonoBehaviour
+{
+    public Transform Target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.LookAt(Target);
+    }
+}
---
+++ UnityProject/Assets/Scripts/UI/InGame/CompassLookAt.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompassLookAt : MonoBehaviour
+{
+    public Transform Target;
+
+    private Vector3 dir;
+    private float angle;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //dir = Target.transform.position - transform.position;
+        //angle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
+        //this.transform.eulerAngles = new Vector3(0, 0, angle);
+        this.transform.LookAt(Target.transform.position);
+        this.transform.rotation.Set(0, 0, this.transform.rotation.z, 0);
+    }
+}
