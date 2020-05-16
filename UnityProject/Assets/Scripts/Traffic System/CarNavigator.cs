5ff77747712d9acc8cac8ea38737535c055c998f
DaanRoobrouck <54914795+DaanRoobrouck@users.noreply.github.com>
2020-05-16 22:01:21 +0200

    CarUpdateProblems

--- UnityProject/Assets/Scripts/Traffic System/CarNavigator.cs
+++ UnityProject/Assets/Scripts/Traffic System/CarNavigator.cs
@@ -23,7 +23,10 @@ public class CarNavigator : MonoBehaviour
 
     void Start()
     {
-        SetDestination(currentWaypoint.GetPosition());
+        if (!_stationary)
+        {
+            SetDestination(currentWaypoint.GetPosition());
+        }
     }
 
     void Update()
